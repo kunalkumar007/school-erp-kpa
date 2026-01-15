cf47686deed895a13553cf0ddb09664b6032160c
Kunal Kumar <55233386+kunalkumar007@users.noreply.github.com>
2026-01-15 21:38:31 +0530

    moved all files from starter

--- demo/src/views/concepts/orders/OrderDetails/components/OrderDetailHeader.tsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from 'react-router-dom'
-
-const OrderDetailHeader = () => {
-    const { id } = useParams()
-
-    return <h3>Order: #{id}</h3>
-}
-
-export default OrderDetailHeader
